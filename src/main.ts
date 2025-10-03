73b5aa0953a083396a7a58f763e4ea74140e567f
adegan1 <150628726+adegan1@users.noreply.github.com>
2025-10-03 02:22:06 +0000

    updated counter

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,12 @@
 console.log("🎮 CMPM 121 - Starting...");
 
 // Simple counter for demonstration
-// deno-lint-ignore prefer-const
 let counter: number = 0;
 
 // Create basic HTML structure
 document.body.innerHTML = `
   <h1>CMPM 121 Project</h1>
-  <p>Counter: <span id="counter">0</span></p>
+  <p>Counter: <span id="counter">${counter}</span></p>
   <button id="increment">Click Me!</button>
 `;
 
@@ -22,5 +21,7 @@ const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
   // This looks like to a good place to add some logic!
+  counter += 1;
+  counterElement.textContent = counter.toString();
   console.log("I have these thingies:", button, counterElement, counter);
 });
